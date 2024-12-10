392645d31cb1615e385fde22919ec69aaef74f94
ptrickill <daniel.diaz.mosquera8@gmail.com>
2024-12-10 00:20:02 -0500

    FlexLayout

--- FlexLayout.xaml.cs
+++ FlexLayout.xaml.cs
@@ -6,4 +6,24 @@ public partial class FlexLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void AbsoluteButton1_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void AbsoluteButton2_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void AbsoluteButton3_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void AbsoluteButton4_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
